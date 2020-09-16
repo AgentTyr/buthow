883a455a484690fde0ebe8a971333e546608950d
agenttyr <richardkostyn@gmail.com>
2020-09-16 12:06:51 -0400

    Possible solution for adding multiple keys to my webserver.

---
+++ Infra/Terraform/hosts.tf
@@ -0,0 +1,8 @@
+// Primarily using digital ocean:
+// https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
+#resource "digitalocean_droplet" "ghost" {
+#  image  = "ubuntu-18-04-x64"
+#  name   = "web-1"
+#  region = "nyc2"
+#  size   = "s-1vcpu-1gb"
+#}
--- Infra/Terraform/ssh_keys.tf
+++ Infra/Terraform/ssh_keys.tf
@@ -1,4 +1,16 @@
-resource "digitalocean_ssh_key" "default" {
-  name       = "Terraform Example"
-  public_key = file("/Users/agenttyr/.ssh/id_rsa.pub")
+// Getting the user AgentTyr for SSHKeys.
+// https://www.terraform.io/docs/providers/github/d/user.html
+data "github_user" "agenttyr" {
+  username = "agenttyr"
+}
+
+// TODO :: There might be an issue with dynamically adding keys.
+// The biggest issue is that each time a key is added, this will result in terraform recreating the pods, which will cause down time.
+// As such, we should have a terraform private key which ansible can then use to add all the keys from GitHub.
+resource "digitalocean_ssh_key" "agenttyr_keys" {
+  // for_each doesn't work with lists, converting to set.
+  for_each   = toset(data.github_user.agenttyr.ssh_keys)
+  // We don't want the key's name to be the public key, so adding a number behind it.
+  name       = "AgentTyr_${index(data.github_user.agenttyr.ssh_keys, each.value) + 1}"
+  public_key = each.value
 }
